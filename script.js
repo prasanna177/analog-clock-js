b811eff3e7b7255be15926e80f46b2210dd5de22
prasanna177 <prasannachand77@gmail.com>
2023-09-26 15:26:00 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+secondsHand = document.getElementById('seconds-hand');
+minutesHand = document.getElementById('minutes-hand');
+hoursHand = document.getElementById('hours-hand');  
+
+function rotate() {
+  date = new Date();
+  seconds = date.getSeconds();
+  minutes = date.getMinutes();
+  hours = date.getHours();
+  secondsHand.style.transform = `rotate(${seconds * 6}deg)`
+  minutesHand.style.transform = `rotate(${minutes * 6 + seconds/10}deg)`
+  hoursHand.style.transform = `rotate(${hours * 30 + minutes/2}deg)`
+}
+
+setInterval(rotate, 100);
+
+let value;
+
+do {
+  value = prompt('Enter a number > 100')
+} while (value < 100)
